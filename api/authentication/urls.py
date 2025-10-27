423b740b74856b34a3993107b40d02a4dab31744
Maxamed459 <maxamedmahdi459@gmail.com>
2025-10-27 21:29:18 +0300

    added new api endpoints

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home"),
+    path('', views.Index.as_view(), name="home"),
     path('register', views.UserRegistration.as_view(), name="register"),
     path('login', views.UserLogin.as_view(), name="login"),
     path('logout', views.logout, name="logout"),
