487c9c83c19af71b5dcfdb8e0f4f43ce39095b4d
Maxamed459 <maxamedmahdi459@gmail.com>
2025-10-30 21:48:17 +0300

    changed the name of the update & delete endpoint

--- api/task/urls.py
+++ api/task/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("index/", views.index, name="task-index"),
     path("", views.TaskCreateListAPIView.as_view(), name="task-list-create"),
-    path("<int:pk>/", views.TaskDetailAPIView.as_view(), name="task-detail"),
+    path("<int:pk>/", views.TaskDetailAPIView.as_view(), name="task-update-delete"),
 ]
