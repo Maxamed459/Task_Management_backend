a344f924b22c91f478204d7b9d090141a0b2fd40
Maxamed459 <maxamedmahdi459@gmail.com>
2025-11-17 19:16:18 +0300

    add CBV

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import APIView, api_view
+from rest_framework.generics import GenericAPIView
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 from rest_framework import status
-from .serializers import UserLoginSerializer, UserSerializer
+from .serializers import UserLoginSerializer, UserRegistrationSerializer
 from rest_framework.permissions import AllowAny
-from rest_framework.authtoken.models import Token
 
 
-class Index(APIView):
+class Index(GenericAPIView):
     permission_classes = [AllowAny]
     def get(self, request, *args, **kwargs):
         return Response({
@@ -17,40 +14,27 @@ class Index(APIView):
         }, status=status.HTTP_200_OK)
 
 
-class UserRegistration(APIView):
+class UserRegistration(GenericAPIView):
+    serializer_class = UserRegistrationSerializer
     permission_classes = [AllowAny]
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
+        user_data = request.data
+        serializer = self.get_serializer(data = user_data)
+        if serializer.is_valid(raise_exception=True):
             user = serializer.save()
-            user_data = UserSerializer(user).data
-            token, _ = Token.objects.get_or_create(user=user)
             return Response({
-                "user": user_data,
-                "token": token.key,
-                "message": "User created successfully!"
+                "data": user,
+                "message": f"Hi Thunk you for registering, welcome to the Tasky platform."
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class UserLogin(APIView):
+class UserLogin(GenericAPIView):
+    serializer_class = UserLoginSerializer
     permission_classes = [AllowAny]
     def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
-
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            token, _ = Token.objects.get_or_create(user=user)
-            user_data = UserLoginSerializer(user).data
-            return Response({
-                "user": user_data,
-                "token": token.key,
-                "message": "Logged in successfully"
-            })
-        
-        return Response({
-            "message": "Invalid username or password"
-        }, status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+    
         
