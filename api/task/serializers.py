e0c7282afe30fbade4c0290720aaf044d00badb2
Maxamed459 <maxamedmahdi459@gmail.com>
2025-11-19 14:23:52 +0300

    add owner to the retuning json data

--- api/task/serializers.py
+++ api/task/serializers.py
@@ -5,14 +5,16 @@ from rest_framework.validators import UniqueValidator
 class TaskSerializer(serializers.ModelSerializer):
 
     owner = serializers.CharField(read_only=True)
+    def get_owner(self, obj):
+        request = self.context.get("request")
+        username = request.user.username.split("@")
+        return request.user.username.split("@")
 
     class Meta:
         model = Task
         fields = ["owner", "id", "title", "description", "due_date", "priority", "is_completed", "created_at", "updated_at"]
 
-    def get_owner(self, obj):
-        request = self.context.get("request")
-        return request.user.username
+    
         
 
 
