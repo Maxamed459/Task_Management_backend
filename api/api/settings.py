93ee74c7583d7ac10ec5ba9a69e92446e6d2782d
Maxamed459 <maxamedmahdi459@gmail.com>
2025-11-21 20:39:49 +0300

    setup

--- api/api/settings.py
+++ api/api/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'corsheaders',
     'authentication',
-    'task'
+    'task',
+    'silk',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'silk.middleware.SilkyMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
